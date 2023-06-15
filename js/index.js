85d049c88c6510f3dca7047c46467f84bdfc4dfd
김기흥 <ewqdsacxzzaqxswcde@gmail.com>
2023-06-15 23:01:46 +0900

    2023-06-15

---
+++ js/index.js
@@ -0,0 +1,50 @@
+function temp_data1{
+	return 더진국_국밥_하단
+}
+
+
+function temp_data2{
+	return 더브런치하우스
+}
+
+
+function temp_data3{
+	return 하남돼지집_부산하단점
+}
+
+
+function temp_data4{
+	return 봉대박스파게티_동아대점
+}
+
+
+function temp_data5{
+	return 보배반점_하단점
+}
+
+
+function temp_data1{
+	return 더진국_국밥_하단
+}
+
+
+function temp_data2{
+	return 더브런치하우스
+}
+
+
+function temp_data3{
+	return 하남돼지집_부산하단점
+}
+
+
+function temp_data4{
+	return 봉대박스파게티_동아대점
+}
+
+
+function temp_data5{
+	return 보배반점_하단점
+}
+
+
